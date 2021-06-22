843d65eb885b868c2a9ba0290cbc5e60288ca74c
saptarshis3112 <saptarshis3112@gmail.com>
2021-06-22 21:12:59 +0530

    remove backend old ts

--- scraper_backend/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { AuthRoutes } from "./Auth";
-
-const authRoutes: Object | any = new AuthRoutes().getRoutes();
-
-export const routes = [
-  authRoutes,
-];
--- scraper_backend/src/validations/LoginValidation.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from "joi";
-
-export const LoginValidation = {
-  payload: Joi.object({
-    email: Joi.string().email().required().label("Email is required"),
-    password: Joi.string().required().label("Password is required"),
-  }),
-};
